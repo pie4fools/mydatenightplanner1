a1e7b3e12bdac39166714ca7e2e69d8ee1379f53
pie4fools <64813630+pie4fools@users.noreply.github.com>
2023-02-07 08:31:46 -0600

    Fixed about sizing

--- src/components/Aboutpage.jsx
+++ src/components/Aboutpage.jsx
@@ -19,7 +19,7 @@ const Aboutpage = () => {
         whileInView={{ opacity: 1, x: 0 }}
         viewport={{ once:true }}
         className='text-white max-w-[1250px] mt-5 w-full mx-auto flex flex-col justify-center'>
-            <h2 className='text-white text-5xl font-thin px-7 text-center my-5 font-serif italic'><span className='text-red-500'>A</span>bout</h2>
+            <h2 className='text-white text-4xl font-thin px-7 text-center my-5 font-serif italic'><span className='text-red-500'>A</span>bout</h2>
             <p className='text-gray-300 text-xl py-4 font-sans px-7'>You have the date, now what?  My Date Night Planner is here to create an experience you and your date will remember forever.  We take the stress out of planning so that you may enjoy your date night.</p>
             <p className='text-gray-300 text-xl py-4 font-sans px-7'>Our dates are individualized just for you.  From first dates,  renewing the spark dates with your longtime love, or a night out with friends, we are here to make memories from first to forever.</p>
             <p className='text-gray-300 text-xl py-4 font-sans px-7'>We will send you a detailed plan of locations, specify dress requirements, and make reservations if necessary.</p>
